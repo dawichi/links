153d2b7cb6fd67c952b0cb639a9c97fbb508b0cb
David M. Fajardo <51042900+Dawichi@users.noreply.github.com>
2022-09-08 17:35:11 +0200

    format

---
+++ src/lib/data/urls.ts
@@ -0,0 +1,12 @@
+import type { Link } from '../types/Link'
+
+export const links: { [key: string]: Link } = {
+    web: {
+        tag: null,
+        url: new URL('https://dawichi.dev'),
+    },
+    linkedin: {
+        tag: null,
+        url: new URL('https://linkedin.com/in/dawichi'),
+    },
+}
---
+++ src/lib/types/Link.d.ts
@@ -0,0 +1,4 @@
+export type Link = {
+    tag: string | null
+    url: URL
+}
